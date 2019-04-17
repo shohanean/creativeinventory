a4a1048a3c5d546bb9f3cff8b8549c4e64d8aae3
shohanean <hossain_shohan1@live.com>
2019-04-17 17:25:39 +0600

    :boom:

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Our Project
                 </div>
 
                 <div class="links">
